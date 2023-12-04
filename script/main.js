afe5c0bb4c589f85dee4c0af9a7b0adfb9860feb
fab1aly <fabien.aly@gmail.com>
2023-12-04 01:03:25 +0100

    update

--- script/main.js
+++ script/main.js
@@ -1,74 +1,5 @@
 
 
-
-// const element = document.querySelector('#home p');
-
-// const phrase = "Web developer";
-
-// let memo = "";
-
-
-// for (let i = 0; i < phrase.length; i++) {
-
-//     memo = memo + phrase[i]
-
-//     element.textContent = memo;
-// }
-// // const intervalID = setInterval(myCallback, 500,);
-
-// // function chatType(phrase) {
-
-// //     memo = memo + phrase[i]
-
-// //     element.textContent = i;
-// // }
-const element = document.querySelector('#about p');
-const text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, sequi, cupiditate deleniti temporibus ratione placeat autem fugiat nihil voluptatum nesciunt architecto.\nVitae saepe dolores earum dignissimos impedit numquam quas asperiores! Expedita vel commodi eos magnam nemo pariatur ?";
-
-// const element = document.querySelector('#home p');
-// const text = "Web developer";
-let memo = "";
-let index = 0;
-let time = 0;
-
-function typeLetter() {
-    if (index < text.length) {
-        time++
-        if (time % 5 == 0) {
-            memo += text[index++];
-            element.textContent = memo;
-
-        }
-        requestAnimationFrame(typeLetter);// Utilise requestAnimationFrame pour la prochaine lettre
-    }
-}
-
-// Commence l'effet de frappe après un délai initial
-// setTimeout(() => requestAnimationFrame(typeLetter), 2000);
-
-
-
-
-
-// function displayHome() {
-
-//     const home_link = document.querySelector('.nav-home');
-//     home_link.innerHTML = "&#x25CF;";
-//     const about_link = document.querySelector('.nav-about');
-//     about_link.textContent = "About";
-//     const project_link = document.querySelector('.nav-project');
-//     project_link.textContent = "Project";
-//     const contact_link = document.querySelector('.nav-contact');
-//     contact_link.textContent = "Contact";
-
-//     const about_article = document.querySelector('#about');
-//     about_article.style.display = "none";
-//     const project_article = document.querySelector('#project');
-//     project_article.style.display = "none";
-//     const contact_article = document.querySelector('#contact');
-//     contact_article.style.display = "none";
-// }
-
 function displayAbout() {
 
     // const home_link = document.querySelector('.nav-home');
@@ -135,31 +66,28 @@ class PChat extends HTMLElement {
         super();
         // créer l'élément
 
-        this.delay = this.getAttribute('delay') || 1000;
-        this.tick = this.getAttribute('tick')
+        this.delay = this.getAttribute('delay') || 1000;  // 1000ms par défaut
+        this.tick = this.getAttribute('tick');
 
-        this.text = this.innerHTML
+        this.text = this.innerHTML;
         // console.log(this.text);
-
         this.textContent = ``;
     }
 
     connectedCallback() {
-
         // le navigateur appelle cette méthode lorsque l'élément est ajouté au document
         // elle peut-être appelé autant de fois que lélément est ajouté ou supprimé)
         // this.text = this.textContent;
 
-        // console.log("test2");
         let memo = "";
         let index = 0;
         let time = 0;
-        let text = this.text
-        let self = this
+        let text = this.text;
+        let self = this;
 
         function typeLetter() {
             if (index < text.length) {
-                time++
+                time++;
                 if (time % 5 == 0) {
                     memo += text[index++];
                     self.innerHTML = memo;
